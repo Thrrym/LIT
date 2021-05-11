b7e80b2a71c983a414fcbace04be3585f9ba70fc
wj <jannis-wilkening@gmx.de>
2021-05-12 00:12:22 +0200

    Adjust import

--- src/main/java/de/tuberlin/tkn/lit/model/activities/Create.java
+++ src/main/java/de/tuberlin/tkn/lit/model/activities/Create.java
@@ -1,6 +1,6 @@
-package main.java.de.tuberlin.tkn.lit.model;
+package de.tuberlin.tkn.lit.model;
 
-import main.java.de.tuberlin.tkn.lit.model.Activity;
+import de.tuberlin.tkn.lit.model.Activity;
 
 
 public class Create extends Activity {
