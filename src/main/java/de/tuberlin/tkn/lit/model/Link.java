0ef011199da3de5ee3d1368a47d126d82c740144
wj <jannis-wilkening@gmx.de>
2021-05-11 23:22:56 +0200

    Change back to use standard getter/setter

--- src/main/java/de/tuberlin/tkn/lit/model/Link.java
+++ src/main/java/de/tuberlin/tkn/lit/model/Link.java
@@ -13,104 +13,57 @@ public class Link {
 	private int height;
 	private int width;
 	
-	private Link(LinkBuilder builder) {
-		this.href = builder.href;
-		this.id = builder.id;
-		this.name = builder.name;
-		this.hreflang = builder.hreflang;
-		this.mediaType = builder.mediaType;
-		this.rel = builder.rel;
-		this.height = builder.height;
-		this.width = builder.width;
+	public Link() {
 	}
 	
 	public String getHref() {
 		return href;
 	}
-
-	public String getId() {
+	
+        public void setHref(String href) {
+		this.href = href;
+	}
+	
+        public String getId() {
 		return id;
 	}
-
+	public void setId(String id) {
+		this.id = id;
+	}
 	public String getName() {
 		return name;
 	}
-	
+	public void setName(String name) {
+		this.name = name;
+	}
 	public String getHreflang() {
 		return hreflang;
 	}
-
+	public void setHreflang(String hreflang) {
+		this.hreflang = hreflang;
+	}
 	public String getMediaType() {
 		return mediaType;
 	}
-
+	public void setMediaType(String mediaType) {
+		this.mediaType = mediaType;
+	}
 	public String[] getRel() {
 		return rel;
 	}
-
+	public void setRel(String[] rel) {
+		this.rel = rel;
+	}
 	public int getHeight() {
 		return height;
 	}
-
+	public void setHeight(int height) {
+		this.height = height;
+	}
 	public int getWidth() {
 		return width;
 	}
-
-	
-	public static class LinkBuilder {
-		// Required
-		private String href;
-		
-		// Optional
-		private String id;
-		private String name;
-		private String hreflang;
-		private String mediaType;
-		private String[] rel;
-		private int height;
-		private int width;
-		
-		public LinkBuilder(String href) {
-			this.href = href;
-		}
-
-		public LinkBuilder setId(String id) {
-			this.id = id;
-			return this;
-		}
-
-		public LinkBuilder setName(String name) {
-			this.name = name;
-			return this;
-		}
-
-		public LinkBuilder setHreflang(String hreflang) {
-			this.hreflang = hreflang;
-			return this;
-		}
-
-		public LinkBuilder setMediaType(String mediaType) {
-			this.mediaType = mediaType;
-			return this;
-		}
-
-		public LinkBuilder setRel(String[] rel) {
-			this.rel = rel;
-			return this;
-		}
-
-		public LinkBuilder setHeight(int height) {
-			this.height = height;
-			return this;
-		}
-
-		public LinkBuilder setWidth(int width) {
-			this.width = width;
-			return this;
-		}
-		
-		public Link build() {
-			return new Link(this);
-		}
+	public void setWidth(int width) {
+		this.width = width;
 	}
 }
