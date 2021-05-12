e7d112577d621c75c6fac91af24d4f49ab61d139
wj <jannis-wilkening@gmx.de>
2021-05-13 01:17:56 +0200

    Full example3 json

--- src/main/java/de/tuberlin/tkn/lit/model/link/Mention.java
+++
@@ -1,8 +0,0 @@
-package de.tuberlin.tkn.lit.model;
-
-import de.tuberlin.tkn.lit.model.Link;
-
-
-public class Mention extends Link {
-
-}
---
+++ src/main/java/de/tuberlin/tkn/lit/model/objects/Mention.java
@@ -0,0 +1,8 @@
+package de.tuberlin.tkn.lit.model.link;
+
+import de.tuberlin.tkn.lit.model.objects.Link;
+
+
+public class Mention extends Link {
+
+}
