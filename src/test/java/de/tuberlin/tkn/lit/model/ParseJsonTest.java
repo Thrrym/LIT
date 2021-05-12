76e58c4a80b4bc86b90fe4c08e82ecf859ebbac2
wj <jannis-wilkening@gmx.de>
2021-05-12 14:58:46 +0200

    Add first functional test

--- src/test/java/de/tuberlin/tkn/lit/model/ParseJsonTest.java
+++ src/test/java/de/tuberlin/tkn/lit/model/ParseJsonTest.java
@@ -1,16 +1,36 @@
-package de.tuberlin.tkn.lit;
+package de.tuberlin.tkn.lit.model;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
 
-class ParseJsonTest {
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import de.tuberlin.tkn.lit.model.objects.Note;
+
+
+class ParseJsonTest {
+//  "@context": "https://www.w3.org/ns/activitystreams",
+        /*  "actor": {
+        "type": "Person",
+                "name": "Sally"
+    }, */
     @Test
-    void testExampleJson() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        File file = new File("activity.json");
+    void testExampleJson() throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        // Ignore @context
+        ObjectMapper objectMapper = new ObjectMapper();//.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        File file = new File(classLoader.getResource("activity.json").getFile());
+
         Activity activity = objectMapper.readValue(file, Activity.class);
-        // assert
+        assertEquals(activity.getObject().getName(), "A Note");
+        MatcherAssert.assertThat(activity.getObject(), instanceOf(Note.class));
     }
 }
