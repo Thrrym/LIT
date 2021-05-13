a21500b9a22415064034d78a59c3678827b9a87c
wj <jannis-wilkening@gmx.de>
2021-05-13 02:18:11 +0200

    Add more types

--- src/main/java/de/tuberlin/tkn/lit/model/ActivityPubObject.java
+++ src/main/java/de/tuberlin/tkn/lit/model/ActivityPubObject.java
@@ -2,17 +2,53 @@ package de.tuberlin.tkn.lit.model;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import de.tuberlin.tkn.lit.model.actors.Person;
-import de.tuberlin.tkn.lit.model.objects.Note;
+import de.tuberlin.tkn.lit.model.activities.*;
+import de.tuberlin.tkn.lit.model.actors.*;
+import de.tuberlin.tkn.lit.model.objects.*;
 
-import java.util.HashMap;
 import java.net.URI;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME,
         property = "type")
 @JsonSubTypes( value = {
+        @JsonSubTypes.Type(value = Article.class, name = "Article"),
+        @JsonSubTypes.Type(value = Document.class, name = "Document"),
+        @JsonSubTypes.Type(value = Event.class, name = "Event"),
+        @JsonSubTypes.Type(value = Image.class, name = "Image"),
         @JsonSubTypes.Type(value = Note.class, name = "Note"),
+        @JsonSubTypes.Type(value = Page.class, name = "Page"),
+        @JsonSubTypes.Type(value = Place.class, name = "Place"),
+        @JsonSubTypes.Type(value = Tombstone.class, name = "Tombstone"),
+
+        @JsonSubTypes.Type(value = Application.class, name = "Application"),
+        @JsonSubTypes.Type(value = Group.class, name = "Group"),
+        @JsonSubTypes.Type(value = Organization.class, name = "Organization"),
         @JsonSubTypes.Type(value = Person.class, name = "Person"),
+        @JsonSubTypes.Type(value = Service.class, name = "Service"),
+
+        @JsonSubTypes.Type(value = Accept.class, name = "Accept"),
+        @JsonSubTypes.Type(value = Add.class, name = "Add"),
+        @JsonSubTypes.Type(value = Announce.class, name = "Announce"),
+        @JsonSubTypes.Type(value = Block.class, name = "Block"),
+        @JsonSubTypes.Type(value = Create.class, name = "Create"),
+        @JsonSubTypes.Type(value = Delete.class, name = "Delete"),
+        @JsonSubTypes.Type(value = Dislike.class, name = "Dislike"),
+        @JsonSubTypes.Type(value = Flag.class, name = "Flag"),
+        @JsonSubTypes.Type(value = Follow.class, name = "Follow"),
+        @JsonSubTypes.Type(value = Ignore.class, name = "Ignore"),
+        @JsonSubTypes.Type(value = Join.class, name = "Join"),
+        @JsonSubTypes.Type(value = Leave.class, name = "Leave"),
+        @JsonSubTypes.Type(value = Like.class, name = "Like"),
+        @JsonSubTypes.Type(value = Move.class, name = "Move"),
+        @JsonSubTypes.Type(value = Offer.class, name = "Offer"),
+        @JsonSubTypes.Type(value = Question.class, name = "Question"),
+        @JsonSubTypes.Type(value = Read.class, name = "Read"),
+        @JsonSubTypes.Type(value = Reject.class, name = "Reject"),
+        @JsonSubTypes.Type(value = Remove.class, name = "Remove"),
+        @JsonSubTypes.Type(value = Undo.class, name = "Undo"),
+        @JsonSubTypes.Type(value = Update.class, name = "Update"),
+        @JsonSubTypes.Type(value = View.class, name = "View"),
+
         // TODO: Add all possible types here
 })
 public class ActivityPubObject {
