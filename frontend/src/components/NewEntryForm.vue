49bca43a1f170f6b86f74f2bb2621b6c9154f5a8
Max Muehlefeldt <max.muehlefeldt@tu-berlin.de>
2021-05-15 19:04:36 +0200

    Form refinement. Nicer labels, return / emit the entry to be created.

--- frontend/src/components/NewEntryForm.vue
+++ frontend/src/components/NewEntryForm.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
     <!-- Create the form the get the information for the selected type. -->
-    <b-form v-if="showForm">
+    <b-form v-if="showForm" v-on:submit="emitNewEntry">
       <!-- Create the individual field based on the provided objects. -->
-      <b-form-group v-for="field in formContent" :key="field.id" v-bind:label="field.name">
+      <b-form-group
+        v-for="field in formContent"
+        v-bind:key="field.id"
+        v-bind:label="field.label"
+      >
         <b-form-input v-model="field.content" v-bind:required="field.requiered">
         </b-form-input>
       </b-form-group>
@@ -15,6 +19,7 @@
 <script>
 export default {
   name: "NewEntryForm",
+
   props: {
     formContent: {
       requiered: true,
@@ -22,6 +27,12 @@ export default {
     showForm: {
       type: Boolean,
       requiered: true,
+    },
+  },
+
+  methods: {
+    emitNewEntry: function () {
+      this.$emit("entryToBeCreated", this.formContent);
     },
   },
 };
