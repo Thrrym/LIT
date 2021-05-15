edd5210db5e9dc6212b39c69212ee6d79e4c057c
Max Muehlefeldt <max.muehlefeldt@tu-berlin.de>
2021-05-15 17:50:52 +0200

    Make the linter even happier.

--- frontend/src/views/NewEntry.vue
+++ frontend/src/views/NewEntry.vue
@@ -27,6 +27,7 @@ export default {
       formContent: {
         article: {
           author: "",
+          title: "",
         },
       },
     };
@@ -44,5 +45,4 @@ export default {
   },
 };
 </script>
-<style>
-</style>
+<style></style>
