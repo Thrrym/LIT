2822a8ec6eba41059e1d9747b1a79628ceaeda00
Max Muehlefeldt <max.muehlefeldt@tu-berlin.de>
2021-05-15 18:43:45 +0200

    Create form. Based on provided objects are inputs are created.

--- frontend/src/components/NewEntryForm.vue
+++ frontend/src/components/NewEntryForm.vue
@@ -1,5 +1,15 @@
 <template>
-  <div></div>
+  <div>
+    <!-- Create the form the get the information for the selected type. -->
+    <b-form v-if="showForm">
+      <!-- Create the individual field based on the provided objects. -->
+      <b-form-group v-for="field in formContent" :key="field.id" v-bind:label="field.name">
+        <b-form-input v-model="field.content" v-bind:required="field.requiered">
+        </b-form-input>
+      </b-form-group>
+      <b-button type="submit" variant="primary">Create new entry</b-button>
+    </b-form>
+  </div>
 </template>
 
 <script>
@@ -7,6 +17,10 @@ export default {
   name: "NewEntryForm",
   props: {
     formContent: {
+      requiered: true,
+    },
+    showForm: {
+      type: Boolean,
       requiered: true,
     },
   },
