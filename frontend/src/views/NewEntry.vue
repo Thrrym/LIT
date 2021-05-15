c635380902532bfae822d02062bf49d7ca7606d1
Max Muehlefeldt <max.muehlefeldt@tu-berlin.de>
2021-05-15 17:12:46 +0200

    Component: Create a new entry. Contains subcomponents to select type of entry and create the form.

--- frontend/src/views/NewEntry.vue
+++ frontend/src/views/NewEntry.vue
@@ -1,13 +1,44 @@
 <template>
-  <div></div>
+  <div>
+    <TypeSelector
+        :entryTypes="entryTypes"
+        v-on:selectedType="setSelectedType"
+    >
+    </TypeSelector>
+  </div>
 </template>
 
 <script>
+import TypeSelector from "@/components/TypeSelector.vue"
+
 export default {
-    name: "New Entry",
+    name: "NewEntry",
+    components: {
+        TypeSelector,
+    },
+    data() {
+        return {
+            selectedType: "",
+            entryTypes: [
+                {type: "article"},
+                {type: "book"},
+                {type: "incollection"},
+            ],
+            formContent: {
+                article: {
+                    author: "",
+                },
+            },
+
+        }
+    },
+    methods: {
+        setSelectedType: function(type) {
+            // Type as selected by the selector. To be set as variable in the NewEntry component.
+            this.selectedType = type;
+        }
+    }
 }
 </script>
-
 <style>
-
 </style>
