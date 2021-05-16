96c3677bf889281a70ef079c691d9b17a14203ab
wj <jannis-wilkening@gmx.de>
2021-05-17 00:06:09 +0200

    Remove json annotation

--- src/main/java/de/tuberlin/tkn/lit/model/Actor.java
+++ src/main/java/de/tuberlin/tkn/lit/model/Actor.java
@@ -1,14 +1,6 @@
 package de.tuberlin.tkn.lit.model;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import de.tuberlin.tkn.lit.deserializer.ActorDeserializer2;
-
-@JsonTypeInfo(use = JsonTypeInfo.Id.NONE)
 public class Actor extends LitObject {
-
-    // TODO:  Attribute and getter/setter https://www.w3.org/TR/activitypub/#actors
 
     private static final String type = "Actor";
 
