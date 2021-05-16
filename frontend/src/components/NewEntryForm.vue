dab1b87839dd68c1e1baa8173c6bc4f08dead2b5
Max Muehlefeldt <max.muehlefeldt@tu-berlin.de>
2021-05-16 23:49:42 +0200

    Optional Fields introduced. Optional fields of the formshown if button pressed. Introduces also associated computed values and methods.

--- frontend/src/components/NewEntryForm.vue
+++ frontend/src/components/NewEntryForm.vue
@@ -2,17 +2,36 @@
   <div>
     <!-- Create the form the get the information for the selected type. -->
     <b-form v-if="showForm" v-on:submit="emitNewEntry">
-      <!-- Create the individual field based on the provided objects. -->
-      <b-form-group
-        v-for="field in formContent"
-        v-bind:key="field.id"
-        v-bind:label="field.label"
-      >
-        <b-form-input
-          v-model="field.content"
-          v-bind:required="field.requiered"
-        ></b-form-input>
+      <b-form-group label="Requiered fields">
+        <!-- Create the individual field based on the provided objects. -->
+        <b-form-group
+          v-for="field in getRequieredFields"
+          v-bind:key="field.id"
+          v-bind:label="field.label"
+        >
+          <b-form-input
+            v-model="field.content"
+            v-bind:required="field.requiered"
+          ></b-form-input>
+        </b-form-group>
       </b-form-group>
+
+      <b-button variant="outline-primary" v-if="showOptionalFieldsButton" v-on:click="setShowOptionalFields">Advanced</b-button>
+
+      <!-- Optional Fields. -->
+      <b-form-group v-if="showOptionalFields" label="Optional Fields">
+        <b-form-group
+          v-for="field in getOptionalFields"
+          v-bind:key="field.id"
+          v-bind:label="field.label"
+        >
+          <b-form-input
+            v-model="field.content"
+            v-bind:required="field.requiered"
+          ></b-form-input>
+        </b-form-group>
+      </b-form-group>
+
       <b-button type="submit" variant="primary">Create new entry</b-button>
     </b-form>
   </div>
@@ -32,9 +51,35 @@ export default {
     },
   },
 
+  data () {
+    return {
+      showOptionalFields: false,
+    }
+  },
+
+  computed: {
+    getRequieredFields: function () {
+      return this.formContent.filter(function (elem) {
+        if (elem.requiered === true) return true;
+      });
+    },
+    getOptionalFields: function () {
+      return this.formContent.filter(function (elem) {
+        if (elem.requiered === false) return true;
+      });
+    },
+    showOptionalFieldsButton: function () {
+      if (this.getRequieredFields.length === 0) return false;
+      else return true;
+    }
+  },
+
   methods: {
     emitNewEntry: function () {
       this.$emit("entryToBeCreated", this.formContent);
+    },
+    setShowOptionalFields: function () {
+      this.showOptionalFields = !this.showOptionalFields;
     },
   },
 };
