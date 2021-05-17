2e8b01b78d8e28023406ac694d51ab88bd2034d6
wj <jannis-wilkening@gmx.de>
2021-05-17 16:18:16 +0200

    Add constructor

--- src/main/java/de/tuberlin/tkn/lit/model/actors/Group.java
+++ src/main/java/de/tuberlin/tkn/lit/model/actors/Group.java
@@ -5,6 +5,11 @@ import de.tuberlin.tkn.lit.model.Actor;
 public class Group extends Actor {
     private static final String type = "Group";
 
+    public Group() {}
+
+    public Group(Actor actor) { super(actor); }
+
+    @Override
     public String getType() {
         return type;
     }
