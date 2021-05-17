2c402e79eaf303a4f0e23565531dbf7fef56374e
wj <jannis-wilkening@gmx.de>
2021-05-17 20:48:43 +0200

    Change to LinkOrObject

--- src/main/java/de/tuberlin/tkn/lit/model/CollectionPage.java
+++ src/main/java/de/tuberlin/tkn/lit/model/CollectionPage.java
@@ -1,14 +1,16 @@
 package de.tuberlin.tkn.lit.model;
 
+import java.util.List;
+
 public class CollectionPage extends LitCollection{
 
 
 
-    public CollectionPage(int totalItems, LitObject[] first, LitObject[] last, LitObject[] current) {
+    public CollectionPage(int totalItems, LinkOrObject first, LinkOrObject last, LinkOrObject current) {
         super(totalItems, first, last, current);
     }
 
-    public CollectionPage(LitObject[] items, int totalItems, LitObject[] first, LitObject[] last, LitObject[] current) {
+    public CollectionPage(List<LinkOrObject> items, int totalItems, LinkOrObject first, LinkOrObject last, LinkOrObject current) {
         super(items, totalItems, first, last, current);
     }
 }
--- src/main/java/de/tuberlin/tkn/lit/model/OrderedCollection.java
+++ src/main/java/de/tuberlin/tkn/lit/model/OrderedCollection.java
@@ -1,10 +1,12 @@
 package de.tuberlin.tkn.lit.model;
 
+import java.util.List;
+
 public class OrderedCollection extends LitCollection {
 
-    private LitObject[] orderedItems;
+    private List<LinkOrObject> orderedItems;
 
-    public OrderedCollection(LitObject[] orderedItems, int totalItems, LitObject[] first, LitObject[] last, LitObject[] current) {
+    public OrderedCollection(List<LinkOrObject> orderedItems, int totalItems, LinkOrObject first, LinkOrObject last, LinkOrObject current) {
         super(totalItems, first, last, current);
         this.orderedItems=orderedItems;
     }
