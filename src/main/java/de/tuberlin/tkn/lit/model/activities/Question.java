2fbd0912a30c15c6cf4fa833314eabab76e22d1a
wj <jannis-wilkening@gmx.de>
2021-05-17 20:53:57 +0200

    Extends from Activity

--- src/main/java/de/tuberlin/tkn/lit/model/activities/Question.java
+++ src/main/java/de/tuberlin/tkn/lit/model/activities/Question.java
@@ -1,18 +1,14 @@
 package de.tuberlin.tkn.lit.model.activities;
 
 import de.tuberlin.tkn.lit.model.Activity;
-import de.tuberlin.tkn.lit.model.IntransitiveActivity;
 
-public class Question extends IntransitiveActivity {
+public class Question extends Activity {
     private static final String type = "Question";
 
-    public Question() {
-    }
+    public Question() {}
 
-    public Question(Activity activity) {
-        super(activity);
-    }
-    public String getType() {
-        return type;
-    }
+    public Question(Activity activity) { super(activity); }
+
+    public String getType() { return type; }
 }
+
