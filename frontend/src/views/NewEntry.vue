07025506e1ce353f3b5d6f649098295f36706f8d
Max Muehlefeldt <max.muehlefeldt@tu-berlin.de>
2021-05-18 18:13:17 +0200

    Introduces new modal. Trigger modal on pressing 'Create new entry'.

--- frontend/src/views/NewEntry.vue
+++ frontend/src/views/NewEntry.vue
@@ -11,19 +11,23 @@
       v-on:entryToBeCreated="setEntryToBeCreated"
     >
     </NewEntryForm>
+    <NewEntryModal ref="modal" v-bind:newEntry="newEntry" v-bind:selectedType="selectedType"></NewEntryModal>
   </div>
 </template>
 
 <script>
 import TypeSelector from "@/components/TypeSelector.vue";
 import NewEntryForm from "@/components/NewEntryForm.vue";
+import NewEntryModal from "@/components/NewEntryModal.vue";
 import newEntryFormContent from "@/js_files/newEntryFormContent.js"; // Import the form contents from seperate JS file.
 
 export default {
   name: "NewEntry",
+
   components: {
     TypeSelector,
     NewEntryForm,
+    NewEntryModal,
   },
 
   data() {
@@ -31,6 +35,7 @@ export default {
       selectedType: "",
       newEntry: "",
       formContent: newEntryFormContent.allTypes,
+      showModal: false,
     };
   },
 
@@ -50,7 +55,7 @@ export default {
     },
     activateForm: function () {
       // Determine if the form for the selected can be shown.
-      // Np type seleceted -> false.
+      // No type seleceted -> false.
       if (this.selectedType === "") return false;
       else return true;
     },
@@ -63,6 +68,12 @@ export default {
     },
     setEntryToBeCreated: function (newEntry) {
       this.newEntry = newEntry;
+      this.triggerModal();
+    },
+    triggerModal: function () {
+      // Show the modal.
+      // For user feedback.
+      this.$refs.modal.showNewEntryModal();
     },
   },
 };
