2b05147fc9ff4fef46c8d4f14085441ef5333d64
yesoer <luisenglaender@web.de>
2021-05-21 12:22:12 +0200

    add example property access

--- src/main/java/de/tuberlin/tkn/lit/controller/ServerController.java
+++ src/main/java/de/tuberlin/tkn/lit/controller/ServerController.java
@@ -1,6 +1,7 @@
 package de.tuberlin.tkn.lit.controller;
 import de.tuberlin.tkn.lit.model.Activity;
 import de.tuberlin.tkn.lit.model.Actor;
+import de.tuberlin.tkn.lit.model.LinkOrObject;
 import de.tuberlin.tkn.lit.model.OrderedCollection;
 import de.tuberlin.tkn.lit.model.activities.Create;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +19,13 @@ public class ServerController {
 
     @RequestMapping(value = "/{actorname}/inbox", method = RequestMethod.POST)
     public void postInbox(@PathVariable("actorname") String actorname, @RequestBody Activity activity) {
-        System.out.println(activity.getActor().getLink()); // example
+        // example access
+        System.out.println(activity.getActor().getLink());
+        LinkOrObject toZero = activity.getTo().get(0);
+        LinkOrObject toOne = activity.getTo().get(1);
+        System.out.println(toZero.getLink());
+        System.out.println(toOne.getLink());
+        // example access end
     }
 
     @RequestMapping(value = "/{actorname}/outbox", method = RequestMethod.GET)
