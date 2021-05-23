66a6b5cd32f8ff45409b13e1c86c665deb01dbcd
Max Muehlefeldt <max.muehlefeldt@tu-berlin.de>
2021-05-23 20:48:06 +0200

    Modified main import of bootstrap etc. Includes now the icons provided through bootstrap.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,11 +5,14 @@ import App from "./App.vue";
 import router from "./router";
 
 // Import Bootstrap.
-import BootstrapVue from 'bootstrap-vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
 Vue.config.productionTip = false;
 
 new Vue({
