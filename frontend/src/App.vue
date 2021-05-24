e2b5cf5cdede84b7ac12f5311978e0e80b44312c
Max Muehlefeldt <max.muehlefeldt@tu-berlin.de>
2021-05-24 14:05:39 +0200

    File renaming.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -32,7 +32,7 @@ export default {
 <style lang="scss">
   // Import custom SASS variable overrides, or alternatively
   // define your variable overrides here instead
-   @import 'styles/_custom.scss';
+   @import 'styles/custom.scss';
 
   // Import Bootstrap and BootstrapVue source SCSS files
   @import '~bootstrap/scss/bootstrap.scss';
