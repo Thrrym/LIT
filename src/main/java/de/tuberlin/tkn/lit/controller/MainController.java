4fa336dd7ffa7b6ed0ca5e5f6e1661f6d1f883f0
T <m>
2021-06-09 15:53:15 +0200

    init ui for java spring boot

---
+++ src/main/java/de/tuberlin/tkn/lit/controller/MainController.java
@@ -0,0 +1,18 @@
+package de.tuberlin.tkn.lit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+    @Controller
+    public class MainController {
+
+        @RequestMapping(value = "/", method = RequestMethod.GET)
+        public String index(Model model) {
+            // this attribute will be available in the view index.html as a thymeleaf variable
+            model.addAttribute("eventName", "FIFA 2018");
+            // this just means render index.html from static/ area
+            return "index";
+        }
+    }
