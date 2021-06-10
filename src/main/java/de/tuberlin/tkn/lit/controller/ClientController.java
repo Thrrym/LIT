1d4912d3ac71b4b65697c0907aa2b06098033469
T <m>
2021-06-10 17:52:56 +0200

    CORS

--- src/main/java/de/tuberlin/tkn/lit/controller/ClientController.java
+++ src/main/java/de/tuberlin/tkn/lit/controller/ClientController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
+@CrossOrigin
 public class ClientController {
 
     @Autowired
--- src/main/java/de/tuberlin/tkn/lit/controller/MainController.java
+++ src/main/java/de/tuberlin/tkn/lit/controller/MainController.java
@@ -2,10 +2,12 @@ package de.tuberlin.tkn.lit.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
     @Controller
+    @CrossOrigin
     public class MainController {
 
         @RequestMapping(value = "/", method = RequestMethod.GET)
