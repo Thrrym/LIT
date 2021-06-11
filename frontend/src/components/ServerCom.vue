ed7ff3a53f7455eae4ea5e379082e0fac236778f
Max Muehlefeldt <max.muehlefeldt@tu-berlin.de>
2021-06-11 11:16:41 +0200

    Revamped ServerCom component. Now must be used for com to the backend. Reason: Emit event, when http request is complete. Previous usage of diffrerent JS file was not workable.

--- frontend/src/components/ServerCom.vue
+++ frontend/src/components/ServerCom.vue
@@ -3,74 +3,78 @@
 </template>
 
 <script>
+
+import { getUserUrl, getApiUrl, prepareNewEntry, prepareNewEntryJson } from "@/js_files/serverCom.js";
+
 export default {
     name: "ServerCom",
+
     data() {
         return {
-
+            requestResponse: "",
         }
     },
+
     props: {
-        requestType: {
+        selectedType: {
             required: true,
         },
-        requestContent: {
+        newEntry: {
             required: true,
         }
     },
+
     methods: {
         triggerServerCom: function () {
-            // this.HtmlRequest();
-            if (this.requestType === "newEntry") {
-                this.postNewEntry();
-            }
-            return;
-        },
-        postNewEntry: function () {
+            // Get the current user and URL of backend.
+            const backendUrl = getUserUrl().backendUrl;
+            const currentUser = getUserUrl().user;
+
+            // Prepare content of the http request. Removes unused properties.
+            var newEntry = prepareNewEntry(this.selectedType, this.newEntry);
+            var json = prepareNewEntryJson(newEntry, backendUrl, currentUser);
+
+            // Maintaine reference to component with this via a new reference.
+            var component = this
+
             // Create the HTTP Request. Uses xmlhttprequest npm package.
             var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
             var httpRequest = new XMLHttpRequest();
 
-            // How to handle state changes of the request.
-            httpRequest.onreadystatechange = function(){
-                
-                console.log(httpRequest.status);
-                console.log(httpRequest.responseText)
-            };
-            
-            // Send what where:
-            let method = "POST";
-            let url = this.$backendUrl + this.$currentUser + "/" + "outbox";
-            
-            console.log(url)
+            console.log(json)
 
-            httpRequest.open(method, url, true);
+            // Set the HTTP Method. HTTP Request send via Proxy to backend server.
+            let method = "POST";
+            var apiUrl = getApiUrl() + currentUser + "outbox";
+
+            httpRequest.open(method, apiUrl, true);
+            //httpRequest.timeout = 4000;
             httpRequest.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 
-            var jsonObject = {
-                "id": url + "/article" + "/1",
-                "attributedTo": this.$backendUrl + this.$currentUser,
-            };
-            for (const property in this.requestContent) {
-                jsonObject[property] = this.requestContent[property];
-            };
-
-            var jsonMain = {
-                "@context": "https://www.w3.org/ns/activitystreams",
-                "type": "Create",
-                "id": url + "/1",
-                "actor": this.$backendUrl + this.$currentUser,
-                "object": jsonObject
-            };
-
-            console.log(jsonMain)
-
-            //httpRequest.send();
+            httpRequest.onreadystatechange = function () {
+                console.log("readystatechange" + httpRequest.readyState)
+                if (httpRequest.readyState === httpRequest.DONE) {
+                    let status = httpRequest.status;
+                    if ((status === 0 || (status >= 200 && status < 400))) {
+                        // The request has been completed successfully
+                        console.log(httpRequest.responseText);
+                        component.requestResponse = httpRequest;
+                        component.callbackResponse();
+                        
+                    } else {
+                        console.log("error");
+                    }
+                }
+            }
+            httpRequest.send(JSON.stringify(json));
         },
-        HtmlRequest: function () {
-            return;
+    
+
+        callbackResponse: function () {
+            console.log("callbackResponse")
+            console.log(this.requestResponse);
+            this.$emit("requestResponse", this.requestResponse);
         },
-        
     },
 }
 </script>
