b10387cfa1b2972e5d308f393566a3cf17051d1c
yesoer <luisenglaender@web.de>
2021-06-13 15:05:18 +0200

    json handling for ordered collections

--- src/main/java/de/tuberlin/tkn/lit/model/OrderedCollection.java
+++ src/main/java/de/tuberlin/tkn/lit/model/OrderedCollection.java
@@ -1,11 +1,21 @@
 package de.tuberlin.tkn.lit.model;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import de.tuberlin.tkn.lit.serializer.ArraySerializer;
+import de.tuberlin.tkn.lit.deserializer.ArrayDeserializer;
 
 import java.util.List;
 
 public class OrderedCollection extends LitCollection {
 
-    private final List<LinkOrObject> orderedItems;
+    private List<LinkOrObject> orderedItems;
 
+    public OrderedCollection() {
+        super();
+    }
+    
     public OrderedCollection(List<LinkOrObject> orderedItems, int totalItems, LinkOrObject first, LinkOrObject last, LinkOrObject current) {
         super(totalItems, first, last, current);
         this.orderedItems = orderedItems;
@@ -19,5 +29,17 @@ public class OrderedCollection extends LitCollection {
 
     public List<LinkOrObject> getOrderedItems() {
         return orderedItems;
+    }
+
+    //serialize
+    @JsonGetter("orderedItems")
+    public List<String> getJson() throws JsonProcessingException {
+        return ArraySerializer.serialize(this.orderedItems);
+    }
+
+    // deserialize
+    @JsonSetter("orderedItems")
+    public void setJson(JsonNode s) throws JsonProcessingException {
+        orderedItems = ArrayDeserializer.deserialize(s);
     }
 }
