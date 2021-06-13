5a2efd89a63fab0360d34417ae83b0100d21c6e4
Max Muehlefeldt <max.muehlefeldt@tu-berlin.de>
2021-06-13 20:29:07 +0200

    Component to get object based on provided URL.

---
+++ frontend/src/components/ServerComGetObject.vue
@@ -0,0 +1,85 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { getApiUrl } from "@/js_files/serverCom.js";
+
+export default {
+    name: "ServerComGetObject",
+
+    data() {
+        return {
+            requestResponse: "",
+        }
+    },
+
+    props: {
+    },
+
+    methods: {
+        triggerGetObject: function (objectUrl) {
+            // const apiObjectUrl = this.getApiObjectUrl(objectUrl);
+
+            // Get the current user and URL of backend.
+            // const backendUrl = getUserUrl().backendUrl;
+            // const currentUser = getUserUrl().user;
+
+            // Maintaine reference to this component with `this` via a new reference.
+            // Reason: Within httpRequest.onreadystatechange the reference changes to httpRequest.
+            var component = this
+
+            // Create the HTTP Request. Uses xmlhttprequest npm package.
+            var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+            var httpRequest = new XMLHttpRequest();
+
+            // Set the HTTP Method. HTTP Request send via Proxy to backend server.
+            let method = "GET";
+            // var apiUrl = getApiUrl() + currentUser + objectUrl;
+            var apiUrl = this.getApiObjectUrl(objectUrl);
+
+            httpRequest.open(method, apiUrl, true);
+            httpRequest.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+            httpRequest.onreadystatechange = function () {
+                // How to react on change of the HTTP request.
+                if (httpRequest.readyState === httpRequest.DONE) {
+                    // HTTP request is finished.
+                    let status = httpRequest.status;
+                    if ((status === 0 || (status >= 200 && status < 400))) {
+                        // The request has been completed successfully.
+                        component.requestResponse = httpRequest;
+                        // Trigger event.
+                        component.callbackResponse();
+                    } else {
+                        // Error handling.
+                        component.callbackError();
+                    }
+                }
+            }
+            httpRequest.send(); // Send the HTTP request with no payload.
+        },
+    
+        callbackResponse: function () {
+            // Function triggered by the onreadystatechange from the HTTP request.
+            // Emits event to parent component to pass result of the HTTP request back upstream.
+            this.$emit("requestResponse", this.requestResponse);
+        },
+
+        callbackError: function () {
+            // Function triggered by the onreadystatechange from the HTTP request.
+            // Emits error to parent component back upstream.
+            this.$emit("requestResponse", "error");
+        },
+
+        getApiObjectUrl: function (objectUrl) {
+            // Construct correct API URL based on provided identifing URL of the object.
+            let apiUrl = getApiUrl()            
+            return apiUrl + objectUrl.split("/").slice(1).slice(-2).join("/")
+        },
+    },
+}
+</script>
+
+<style>
+</style>
