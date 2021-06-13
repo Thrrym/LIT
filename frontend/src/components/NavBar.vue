c3f94eb74edf1e110e28f149cc73918d23dba2eb
Max Muehlefeldt <max.muehlefeldt@tu-berlin.de>
2021-06-13 11:19:02 +0200

    Renamed + to New Entry in Navbar.

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -12,7 +12,7 @@
       <!-- <b-nav-item><router-link to="/">Lit</router-link></b-nav-item> | -->
       <b-nav-item to="/about">About</b-nav-item>
       <b-nav-item to="/debug">Debug</b-nav-item>
-      <b-nav-item to="/new-entry">+</b-nav-item>
+      <b-nav-item to="/new-entry">New Entry</b-nav-item>
       <b-nav-item to="/signin">Login</b-nav-item>
       <b-nav-item to="/profile">Profile</b-nav-item>
       <b-nav-item to="/search">Search</b-nav-item>
