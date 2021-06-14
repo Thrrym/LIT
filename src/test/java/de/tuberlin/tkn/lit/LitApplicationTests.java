1ef1b649011e83302dad756daa697ff5ad1820fc
Mariu Krause <mariuskrause90@gmail.com>
2021-06-14 15:47:00 +0200

    fixed tests

--- src/test/java/de/tuberlin/tkn/lit/LitApplicationTests.java
+++ src/test/java/de/tuberlin/tkn/lit/LitApplicationTests.java
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class LitApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
