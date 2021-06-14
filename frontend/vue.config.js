cc12f9226a23bd138c5c74119f626e19632ff4f9
Max Muehlefeldt <max.muehlefeldt@tu-berlin.de>
2021-06-14 10:51:32 +0200

    Proxy config more robust. Remaining issue with very first http request.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,10 +1,13 @@
 module.exports = {
     devServer: {
       proxy: {
+        // For full options see https://github.com/chimurai/http-proxy-middleware#options
         '^/api': {
-          target: 'http://localhost:8080/',
-          ws: true,
-          changeOrigin: true,
+          target: 'http://127.0.0.1:8080/',
+          timeout: 6000, // Timeout (in millis) for incoming requests.
+          ws: true, // Proxy websockets.
+          secure: false, // Want to verify the SSL Certs.
+          changeOrigin: true, // Changes the origin of the host header to the target URL.
           pathRewrite: {
             '^/api': ''
           }
