64ef3e24800530d82353668c0079327ba84aa302
Max Muehlefeldt <max.muehlefeldt@tu-berlin.de>
2021-06-15 11:37:02 +0200

    New store variable to setup APIs for backend properly.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import Vuex from 'vuex'
+import Vuex from "vuex";
 import App from "./App.vue";
 
 // Import the router for vue.
@@ -17,42 +17,45 @@ Vue.config.productionTip = false;
 // Using Vuex: Global variables to switch between user and backend.
 const store = new Vuex.Store({
   state: {
-    currentUser: "testuser01/", // The active "loggend in" user.
+    currentUser: "testuser01/", // The active "logged in" user.
     backendUrl: "http://localhost:8080/", // The backend URL.
-    proxyBackendUrl: "http://127.0.0.1:" + location.port + "/api8080/", // The URL of the proxy between the frontend and backend.
+    // The URL of the proxy between the frontend and backend.
+    proxyBackendUrl: "http://localhost:" + location.port + "/api8080/",
+    otherProxyBackendUrl: "http://localhost:" + location.port + "/api8081/",
   },
   mutations: {
-    setUser01 (state) {
+    setUser01(state) {
       state.currentUser = "testuser01/";
     },
-    setUser02 (state) {
+    setUser02(state) {
       state.currentUser = "testuser02/";
     },
-    setBackend01 (state) {
+    setBackend01(state) {
       state.backendUrl = "http://localhost:8080/";
-      state.proxyBackendUrl = "http://127.0.0.1:" + location.port + "/api8080/";
+      state.proxyBackendUrl = "http://localhost:" + location.port + "/api8080/";
+      state.otherProxyBackendUrl = "http://localhost:" + location.port + "/api8081/";
     },
-    setBackend02 (state) {
+    setBackend02(state) {
       state.backendUrl = "http://localhost:8081/";
-      state.proxyBackendUrl = "http://127.0.0.1:" + location.port + "/api8081/";
+      state.proxyBackendUrl = "http://localhost:" + location.port + "/api8081/";
+      state.otherProxyBackendUrl = "http://localhost:" + location.port + "/api8080/";
     },
   },
   methods: {
     setUser01() {
-      this.$store.commit('setUser01');
+      this.$store.commit("setUser01");
     },
     setUser02() {
-      this.$store.commit('setUser02');
+      this.$store.commit("setUser02");
     },
     setBackend01() {
-      this.$store.commit('setBackend01');
+      this.$store.commit("setBackend01");
     },
     setBackend02() {
-      this.$store.commit('setBackend02');
+      this.$store.commit("setBackend02");
     },
   },
-})
-
+});
 
 new Vue({
   router,
