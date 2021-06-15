47e7c4e5d09c6978499ef275357391aeef854d53
Max Muehlefeldt <max.muehlefeldt@tu-berlin.de>
2021-06-15 12:31:21 +0200

    Basic Card implemntation to be used on landing page.

--- frontend/src/views/Debug.vue
+++ frontend/src/views/Debug.vue
@@ -6,26 +6,85 @@
 
     <b-form @submit="onSubmitGetObject">
       <b-form-group label="Object URL" label-for="input-1">
-        <b-form-input id="input-1" v-model="objectUrl" required placeholder="Object">
+        <b-form-input
+          id="input-1"
+          v-model="objectUrl"
+          required
+          placeholder="Object"
+        >
         </b-form-input>
-      <b-button type="submit" variant="primary">GET Object</b-button>
+        <b-button type="submit" variant="primary">GET Object</b-button>
       </b-form-group>
     </b-form>
 
     <b-form @submit="onSubmitLikeObject">
       <b-form-group label="Object URL" label-for="input-2">
-        <b-form-input id="input-2" v-model="objectUrl" required placeholder="Object">
+        <b-form-input
+          id="input-2"
+          v-model="objectUrl"
+          required
+          placeholder="Object"
+        >
         </b-form-input>
-      <b-button type="submit" variant="primary">Like Object</b-button>
+        <b-button type="submit" variant="primary">Like Object</b-button>
       </b-form-group>
     </b-form>
 
-    <ServerComGetInbox ref="inbox" v-on:requestResponse="setRequestResponse"></ServerComGetInbox>
-    <ServerComGetObject ref="object" v-on:requestResponse="setRequestResponse"></ServerComGetObject>
-    <ServerComLikePost ref="like" v-on:requestResponse="setRequestResponse"></ServerComLikePost>
-    <ServerComGetUserObjects ref="userObjects" v-on:requestResponse="setRequestResponse"></ServerComGetUserObjects>
-    <ServerComGetUserRelevantObjects ref="userRelevantObjects" v-on:requestResponse="setRequestResponse"></ServerComGetUserRelevantObjects>
+    <ServerComGetInbox
+      ref="inbox"
+      v-on:requestResponse="setRequestResponse"
+    ></ServerComGetInbox>
+    <ServerComGetObject
+      ref="object"
+      v-on:requestResponse="setRequestResponse"
+    ></ServerComGetObject>
+    <ServerComLikePost
+      ref="like"
+      v-on:requestResponse="setRequestResponse"
+    ></ServerComLikePost>
+    <ServerComGetUserObjects
+      ref="userObjects"
+      v-on:requestResponse="setRequestResponseUserObjects"
+    ></ServerComGetUserObjects>
+    <ServerComGetUserRelevantObjects
+      ref="userRelevantObjects"
+      v-on:requestResponse="setRequestResponseRelevantUserObjects"
+    ></ServerComGetUserRelevantObjects>
     <p v-text="getResponse"></p>
+
+    <div>
+      <b-card v-for="entry in objectsByUser" v-bind:key="entry.id"
+
+        tag="article"
+        style="max-width: 20rem"
+        class="mb-2"
+      >
+        <b-card-title v-text="entry.author"></b-card-title>
+        <b-card-sub-title v-text="entry.journal"></b-card-sub-title>
+        <b-card-text v-text="entry.title"></b-card-text>
+
+      </b-card>
+    </div>
+
+    <div>
+      <b-card v-for="entry in objectsRelevantToUser" v-bind:key="entry.id"
+
+              tag="article"
+              style="max-width: 20rem"
+              class="mb-2"
+      >
+        <b-card-title v-text="entry.author"></b-card-title>
+        <b-card-sub-title v-text="entry.journal"></b-card-sub-title>
+        <b-card-text v-text="entry.title"></b-card-text>
+
+        <b-button href="#" variant="primary">
+          <b-icon icon="bookmark-heart" v-on:click="likePost(entry.id)"></b-icon>
+        </b-button>
+
+      </b-card>
+    </div>
+
+
   </div>
 </template>
 
@@ -36,14 +95,15 @@ import ServerComLikePost from "@/components/ServerComLikePost.vue";
 import ServerComGetUserObjects from "@/components/ServerComGetUserObjects.vue";
 import ServerComGetUserRelevantObjects from "@/components/ServerComGetUserRelevantObjects.vue";
 
-
 export default {
   name: "Debug",
   data() {
     return {
       requestResponse: "",
       objectUrl: "",
-    }
+      objectsByUser: "",
+      objectsRelevantToUser: "",
+    };
   },
 
   components: {
@@ -63,16 +123,26 @@ export default {
       // Handle the event triggered by the ServerComGetInbox component.
       this.requestResponse = response;
     },
-    onSubmitGetObject: function() {
+    setRequestResponseUserObjects: function (response) {
+      this.setRequestResponse(response);
+      this.objectsByUser = JSON.parse(this.getResponse).orderedItems;
+      console.log("objectsByUser", this.objectsByUser);
+    },
+    setRequestResponseRelevantUserObjects: function (response) {
+      this.setRequestResponse(response);
+      this.objectsRelevantToUser = JSON.parse(this.getResponse).orderedItems;
+      console.log("objectsByUser", this.objectsByUser);
+    },
+    onSubmitGetObject: function () {
       this.getObject();
     },
-    getObject: function() {
+    getObject: function () {
       this.$refs.object.triggerGetObject(this.objectUrl);
     },
     onSubmitLikeObject: function () {
       this.likeObject();
     },
-    likeObject: function() {
+    likeObject: function () {
       this.$refs.like.triggerLikePost(this.objectUrl);
     },
     getUserObjects: function () {
@@ -80,8 +150,10 @@ export default {
     },
     getUserRelevantObjects: function () {
       this.$refs.userRelevantObjects.triggerGetRelevantObjects();
-    }
-
+    },
+    likePost: function (id) {
+      this.$refs.like.triggerLikePost(id);
+    },
   },
 
   computed: {
@@ -89,14 +161,11 @@ export default {
       // Facilitate print of the Inbox.
       if (this.requestResponse === "") {
         return "";
-      };
+      }
       return this.requestResponse.responseText;
     },
-  }
-
-
-}
+  },
+};
 </script>
 
-<style>
-</style>
+<style></style>
