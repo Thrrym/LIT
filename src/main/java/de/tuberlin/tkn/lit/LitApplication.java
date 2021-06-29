3e7baa2f585cdf088fe64df1acbfdd320b2c1291
yesoer <luisenglaender@web.de>
2021-06-29 19:26:15 +0200

    fix build

--- src/main/java/de/tuberlin/tkn/lit/LitApplication.java
+++ src/main/java/de/tuberlin/tkn/lit/LitApplication.java
@@ -1,5 +1,7 @@
 package de.tuberlin.tkn.lit;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
