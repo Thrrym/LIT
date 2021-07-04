c5c2c638295f3623f602499785d336ac807d2c2f
yesoer <luisenglaender@web.de>
2021-07-04 21:03:19 +0200

    remove unused import

--- src/main/java/de/tuberlin/tkn/lit/controller/ServerController.java
+++ src/main/java/de/tuberlin/tkn/lit/controller/ServerController.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashSet;
 import java.util.List;
 
 @RestController
