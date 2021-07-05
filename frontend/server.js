f4e825ee45bd54285f92010fe4976f049bca7e2b
Max Muehlefeldt <max.muehlefeldt@tu-berlin.de>
2021-07-05 11:09:42 +0200

    Node server: Wildcard to match proxy path. Moved proxy options to const. Cleaner proxy setup.

--- frontend/server.js
+++ frontend/server.js
@@ -14,42 +14,45 @@ const hostname = "localhost";
 const frontendPort = parseInt(args[0]);
 
 // Load and use the same proxy as for the development server.
+const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
-let express = require("express");
-let serveStatic = require("serve-static");
+
+//let path = require("path");
+//let serveStatic = require("serve-static");
 let app = express();
 
 // Setup the proxy: Forward backend requests from the frontend to the backend server.
-app.use(
-  "/api",
-  createProxyMiddleware({
-    target: "http://localhost:8081/", // Is required! Router updates the target based on proxy handle.
-    router: function (req) {
-      // Update the target. Where does the request needs to be sent?
-      // Decision based on the proxy handle. Example '/api8080'.
-      const apiPort = req.originalUrl.split("/")[1].slice(3); // Get the port from the handle.
-      const apiHost = req.headers.host.split(":")[0]; // Get the host. Should always be 'localhost'.
-      const protocol = "http://"; // Set the protocol.
-      console.log("router port", apiPort);
-      // Construct the new target URL: Protocol, host and port.
-      // Here we expect this to be always in the form: 'http://localhost:8080/'
-      let newTarget = protocol + apiHost + ":" + apiPort + "/";
-      console.log("newTarget", newTarget);
-      return newTarget;
-    },
-    timeout: 6000, // Timeout (in millis) for incoming requests.
-    ws: true, // Proxy websockets.
-    secure: false, // Want to verify the SSL Certs.
-    changeOrigin: true, // Changes the origin of the host header to the target URL.
-    pathRewrite: function (path) {
-      // Remove from the path the proxy handle, example '/api8080'. Uses a custom function.
-      let newPath = path.split("/").slice(2).join("/");
-      return newPath;
-    },
-  })
-);
-app.use(serveStatic(__dirname + "/dist"));
+const proxyOptions = {
+  target: "http://localhost:8085/", // Is required! Router updates the target based on proxy handle.
+  router: function (req) {
+    // Update the target. Where does the request needs to be sent?
+    // Decision based on the proxy handle. Example '/api8080'.
+    const apiPort = req.originalUrl.split("/")[1].slice(3); // Get the port from the handle.
+    const apiHost = req.headers.host.split(":")[0]; // Get the host. Should always be 'localhost'.
+    const protocol = "http://"; // Set the protocol.
+    console.log("router port", apiPort);
+    // Construct the new target URL: Protocol, host and port.
+    // Here we expect this to be always in the form: 'http://localhost:8080/'
+    let newTarget = protocol + apiHost + ":" + apiPort + "/";
+    console.log("newTarget", newTarget);
+    return newTarget;
+  },
+  timeout: 6000, // Timeout (in millis) for incoming requests.
+  ws: true, // Proxy websockets.
+  secure: false, // Want to verify the SSL Certs.
+  changeOrigin: true, // Changes the origin of the host header to the target URL.
+  pathRewrite: function (path) {
+    // Remove from the path the proxy handle, example '/api8080'. Uses a custom function.
+    let newPath = path.split("/").slice(2).join("/");
+    console.log("newPath", newPath);
+    return newPath;
+  },
+};
+const proxy = createProxyMiddleware(proxyOptions);
+app.use("/api*", proxy); // Use of wildcard here required?
+
+app.use(express.static(__dirname + "/dist"));
 
 app.listen(frontendPort, hostname, () => {
-  console.log("Frontend server running at http://", hostname, ":", frontendPort);
+  console.log(`Server running at http://${hostname}:${frontendPort}/`);
 });
