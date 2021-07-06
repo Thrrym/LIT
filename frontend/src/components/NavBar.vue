e82f279281df2eac87fd98de76beb829dd4c85bd
Max Muehlefeldt <max.muehlefeldt@tu-berlin.de>
2021-07-06 16:07:56 +0200

    Updated navbar to facilitate sign up and sign in. Launch modals from the navbar to start the processes as needed by the user.

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,32 +1,34 @@
 <template>
+  <div>
+    <b-navbar toggleable="lg" type="light" variant="light">
+      <b-navbar-brand>
+        <router-link to="/">
+          <img src="../assets/lit_logo_v3.svg" class="" />
+        </router-link>
+      </b-navbar-brand>
 
-  <b-navbar toggleable="lg" type="light" variant="light">
+      <b-navbar-toggle target="nav-collapse">
+        <template #default="{ expanded }">
+          <b-icon v-if="expanded" icon="bookmark"></b-icon>
+          <b-icon v-else icon="bookmark-fill"></b-icon>
+        </template>
+      </b-navbar-toggle>
 
-    <b-navbar-brand>
-      <router-link to="/">
-        <img src="../assets/lit_logo_v3.svg" class="">
-      </router-link>
-    </b-navbar-brand>
-
-    <b-navbar-toggle target="nav-collapse">
-      <template #default="{ expanded }">
-        <b-icon v-if="expanded" icon="bookmark"></b-icon>
-        <b-icon v-else icon="bookmark-fill"></b-icon>
-      </template>
-    </b-navbar-toggle>
-
-    <b-collapse id="nav-collapse" is-nav>
-    <b-navbar-nav pills> 
-      <!-- <b-nav-item><router-link to="/">Lit</router-link></b-nav-item> | -->
-<!--      <b-nav-item to="/about">About</b-nav-item>-->
-      <b-nav-item to="/debug">Debug</b-nav-item>
-      <b-nav-item to="/new-entry">New Entry</b-nav-item>
-<!--      <b-nav-item to="/signin">Login</b-nav-item>-->
-      <b-nav-item to="/profile">Profile</b-nav-item>
-      <b-nav-item to="/search">Search</b-nav-item>
-    </b-navbar-nav>
-    <b-navbar-nav class="ml-auto">
-<!--      <b-nav-item-dropdown right>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav pills>
+          <!-- <b-nav-item><router-link to="/">Lit</router-link></b-nav-item> | -->
+          <!--      <b-nav-item to="/about">About</b-nav-item>-->
+          <b-nav-item to="/debug">Debug</b-nav-item>
+          <b-nav-item to="/new-entry">New Entry</b-nav-item>
+          <b-nav-item v-if="!isUserLoggedIn" v-on:sign-up-success="setTestReturn" v-on:click="showSignUpModal">Sign up</b-nav-item>
+<!--          <b-nav-item v-if="!isUserLoggedIn" v-on="eventHandlerSignUp">Sign up</b-nav-item>-->
+          <b-nav-item v-if="!isUserLoggedIn" v-on:click="showSignInModal">Sign in</b-nav-item>
+          <b-nav-item v-if="isUserLoggedIn" v-on:click="logOut">Log out</b-nav-item>
+          <b-nav-item to="/profile">Profile</b-nav-item>
+          <b-nav-item to="/search">Search</b-nav-item>
+        </b-navbar-nav>
+        <b-navbar-nav class="ml-auto">
+          <!--      <b-nav-item-dropdown right>
         &lt;!&ndash; Using 'button-content' slot &ndash;&gt;
         <template #button-content>
           <em v-text="getCurrentBackend"></em>
@@ -35,59 +37,97 @@
         <b-dropdown-item v-on:click="setBackend02" href="#">8081</b-dropdown-item>
       </b-nav-item-dropdown>-->
 
-      <b-nav-item-dropdown right>
-        <!-- Using 'button-content' slot -->
-        <template #button-content>
-          <em v-text="getCurrentUser"></em>
-        </template>
-        <b-dropdown-item v-on:click="setUser1" href="#">testuser01</b-dropdown-item>
-        <b-dropdown-item v-on:click="setUser2" href="#">testuser02</b-dropdown-item>
-      </b-nav-item-dropdown>
-
-    </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
-
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template #button-content>
+              <em v-text="getCurrentUser"></em>
+            </template>
+            <b-dropdown-item v-on:click="setUser1" href="#"
+              >testuser01</b-dropdown-item
+            >
+            <b-dropdown-item v-on:click="setUser2" href="#"
+              >testuser02</b-dropdown-item
+            >
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <SignUpModal ref="SignUpModal"></SignUpModal>
+    <SignInModal ref="SignInModal"></SignInModal>
+  </div>
 </template>
 
 <script>
+import SignUpModal from "@/components/SignUpModal";
+import SignInModal from "@/components/SignInModal";
+
 export default {
-    name: "NavBar",
-    data() {
-      return {
-      }
+  name: "NavBar",
+  components: {
+    SignUpModal,
+    SignInModal,
+  },
+  data() {
+    return {
+      testReturn: "",
+      /*eventHandlerSignUp: {
+        click: this.showSignUpModal,
+        signUpSuccess: this.setTestReturn,
+      },*/
+    };
+  },
+  methods: {
+    // Method to manipulate the vuex store.
+    setUser1: function () {
+      this.$store.commit("setUser01");
     },
-    methods: {
+    setUser2: function () {
       // Method to manipulate the vuex store.
-      setUser1: function () {
-        this.$store.commit("setUser01");
-      },
-      setUser2: function () {
-        // Method to manipulate the vuex store.
-        this.$store.commit("setUser02");
-      },
-      setBackend01: function () {
-        // Set the current backend to option 1.
-        this.$store.commit('setBackend01')
-      },
-      setBackend02: function () {
-        // Set the current backend to option 2.
-        this.$store.commit('setBackend02')
-      },
+      this.$store.commit("setUser02");
     },
-    computed: {
-      getCurrentUser: function () {
-        // Get user from the vuex store an remove last slash.
-        return this.$store.state.currentUser.split("/")[0];
-      },
-      getCurrentBackend: function () {
-        // Get the active backend from the vuex store.
-        return this.$store.state.backendUrl;
+    setBackend01: function () {
+      // Set the current backend to option 1.
+      this.$store.commit("setBackend01");
+    },
+    setBackend02: function () {
+      // Set the current backend to option 2.
+      this.$store.commit("setBackend02");
+    },
+    showSignUpModal: function () {
+      this.testReturn = "Sign up modal start.";
+      this.$refs.SignUpModal.showSignUpModal();
+    },
+    showSignInModal: function () {
+      this.$refs.SignInModal.showSignInModal();
+    },
+    logOut: function () {
+      this.$store.commit("logOut");
+    },
+    setTestReturn: function () {
+      console.log("Hmmmmmmmmmmmmm");
+      this.testReturn = "sign-up-success";
+    },
+    /*eventHandlerSignUp: function (event) {
+      event.preventDefault();
+      if (event.type === "click") {
+        console.log("click");
       }
+    }*/
+  },
+  computed: {
+    getCurrentUser: function () {
+      // Get user from the vuex store an remove last slash.
+      return this.$store.state.currentUser.split("/")[0];
     },
-}
+    getCurrentBackend: function () {
+      // Get the active backend from the vuex store.
+      return this.$store.state.backendUrl;
+    },
+    isUserLoggedIn: function () {
+      return this.$store.state.loginStatus === "loggedIn";
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ frontend/src/components/SignInModal.vue
@@ -0,0 +1,115 @@
+<template>
+  <div>
+    <b-modal ref="modal-1" v-bind:title="modalTitle" @ok="sendSignInToServer">
+      <b-card
+        style="max-width: 20rem"
+        class="mb-2"
+        no-body
+        border-variant="light"
+      >
+        <img
+          src="../assets/lit_logo_v3.svg"
+          alt="Lit Logo"
+          class="card-img card-img-top img-fluid"
+        />
+        <b-card-body>
+          <b-form id="login-form">
+            <!-- Input for Username. -->
+            <b-form-group
+              id="input-group-1"
+              label="Your Username:"
+              label-for="input-1"
+            >
+              <b-form-input
+                id="input-1"
+                v-model="form.username"
+                placeholder="Enter Username"
+                required
+              ></b-form-input>
+            </b-form-group>
+
+            <!-- Input for password. -->
+            <b-form-group
+              id="input-group-2"
+              label="Your password:"
+              label-for="input-2"
+            >
+              <b-form-input
+                id="input-2"
+                v-model="form.password"
+                placeholder="Enter Password"
+                type="password"
+                required
+              ></b-form-input>
+            </b-form-group>
+
+            <!-- Login Button. -->
+            <!--          <b-button type="submit" variant="primary">Login</b-button>-->
+          </b-form>
+        </b-card-body>
+      </b-card>
+    </b-modal>
+
+    <ServerComSignIn
+      ref="ServerComSignIn"
+      v-on:signInSuccess="signInSuccess"
+    ></ServerComSignIn>
+
+    <b-modal ref="welcomeModal" title="Welcome to Lit" ok-only>
+      Hello {{ form.username }}, you are now logged in.
+    </b-modal>
+  </div>
+</template>
+
+<script>
+import ServerComSignIn from "@/components/ServerComSignIn";
+
+export default {
+  name: "SignInModal",
+  components: { ServerComSignIn },
+  data() {
+    return {
+      modalTitle: "Sign in to LIT",
+      form: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+
+  props: {},
+
+  methods: {
+    showSignInModal: function () {
+      // Trigger the modal.
+      this.$refs["modal-1"].show();
+    },
+    sendSignInToServer: function () {
+      this.$refs.ServerComSignIn.triggerSignIn(this.form);
+    },
+/*    signUpSuccess: function () {
+      // Close sign up modal and trigger event to show the sign in modal.
+      this.$refs["modal-1"].hide();
+      this.$emit("signUpSuccess");
+    },*/
+    signInSuccess: function (message) {
+      // Update local storage and the vuex store.
+      localStorage.setItem("token", message.accessToken);
+      localStorage.setItem("user", this.form.username)
+      this.$store.commit("authSuccess");
+
+      // Show the welcome message.
+      this.$refs.welcomeModal.show();
+      },
+  },
+
+  computed: {
+    getModalTitle: function () {
+      // Generate title of the modal.
+      return this.author;
+    },
+  },
+};
+</script>
+
+<style></style>
