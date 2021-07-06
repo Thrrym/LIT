dbc4ce12fe09470d47b8389896261f5084ea993e
Max Muehlefeldt <max.muehlefeldt@tu-berlin.de>
2021-07-06 17:13:26 +0200

    Test user selection removed.

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -20,35 +20,15 @@
           <!--      <b-nav-item to="/about">About</b-nav-item>-->
           <b-nav-item to="/debug">Debug</b-nav-item>
           <b-nav-item to="/new-entry">New Entry</b-nav-item>
-          <b-nav-item v-if="!isUserLoggedIn" v-on:click="showSignUpModal">Sign up</b-nav-item>
-<!--          <b-nav-item v-if="!isUserLoggedIn" v-on="eventHandlerSignUp">Sign up</b-nav-item>-->
-          <b-nav-item v-if="!isUserLoggedIn" v-on:click="showSignInModal('normalSignIn')">Sign in</b-nav-item>
-          <b-nav-item v-if="isUserLoggedIn" v-on:click="logOut">Log out</b-nav-item>
+
+
           <b-nav-item to="/profile">Profile</b-nav-item>
           <b-nav-item to="/search">Search</b-nav-item>
         </b-navbar-nav>
         <b-navbar-nav class="ml-auto">
-          <!--      <b-nav-item-dropdown right>
-        &lt;!&ndash; Using 'button-content' slot &ndash;&gt;
-        <template #button-content>
-          <em v-text="getCurrentBackend"></em>
-        </template>
-        <b-dropdown-item v-on:click="setBackend01" href="#">8080</b-dropdown-item>
-        <b-dropdown-item v-on:click="setBackend02" href="#">8081</b-dropdown-item>
-      </b-nav-item-dropdown>-->
-
-          <b-nav-item-dropdown right>
-            <!-- Using 'button-content' slot -->
-            <template #button-content>
-              <em v-text="getCurrentUser"></em>
-            </template>
-            <b-dropdown-item v-on:click="setUser1" href="#"
-              >testuser01</b-dropdown-item
-            >
-            <b-dropdown-item v-on:click="setUser2" href="#"
-              >testuser02</b-dropdown-item
-            >
-          </b-nav-item-dropdown>
+          <b-nav-item v-if="!isUserLoggedIn" v-on:click="showSignUpModal">Sign up</b-nav-item>
+          <b-nav-item v-if="!isUserLoggedIn" v-on:click="showSignInModal('normalSignIn')">Sign in</b-nav-item>
+          <b-nav-item v-if="isUserLoggedIn" v-on:click="logOut">Log out</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
