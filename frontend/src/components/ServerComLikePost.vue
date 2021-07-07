0035840f9f0f33f65bd5d8b0cc2fa12213716f0b
Max Muehlefeldt <max.muehlefeldt@tu-berlin.de>
2021-07-07 15:04:09 +0200

    Header added to http request for Like. Authentication needed for outbox.

--- frontend/src/components/ServerComLikePost.vue
+++ frontend/src/components/ServerComLikePost.vue
@@ -84,6 +84,7 @@ export default {
         "Content-Type",
         "application/json;charset=UTF-8"
       );
+      httpRequest.setRequestHeader("Authorization", this.$store.getters.getToken);
 
       httpRequest.onreadystatechange = function () {
         // How to react on change of the HTTP request.
