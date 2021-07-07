65acea12279d64ffd1efea3ca95f8273706e3bfe
Max Muehlefeldt <max.muehlefeldt@tu-berlin.de>
2021-07-07 15:05:51 +0200

    Vue main clean-up.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,22 +1,18 @@
 import Vue from "vue";
-// import Vuex from "vuex";
 import App from "./App.vue";
-
 
 // Import the router for vue.
 import router from "./router";
 
-// Import Bootstrap.
+// Import and use bootstrap and icons.
 import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
-
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
-//Vue.use(Vuex);
+
+// Import the Vuex store for global functions and variables.
 import store from "./store.js";
 
 Vue.config.productionTip = false;
-
-
 
 new Vue({
   router,
