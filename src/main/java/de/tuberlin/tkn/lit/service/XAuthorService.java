47a516867f016592a608cc3dab2b17f7b6352da2
generalgroovy <41192316+generalgroovy@users.noreply.github.com>
2021-07-07 23:52:45 +0200

    DB

--- src/main/java/de/tuberlin/tkn/lit/service/XAuthorService.java
+++ src/main/java/de/tuberlin/tkn/lit/service/XAuthorService.java
@@ -1,29 +1,27 @@
 package de.tuberlin.tkn.lit.service;
 
-import de.tuberlin.tkn.lit.model.activitypub.actors.Person;
 import de.tuberlin.tkn.lit.model.lit.Author;
+import de.tuberlin.tkn.lit.model.lit.BibTeXArticle;
 import de.tuberlin.tkn.lit.storage.IAuthorRepository;
-import de.tuberlin.tkn.lit.storage.IPersonRepository;
+import de.tuberlin.tkn.lit.storage.IBibTeXArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class AuthorService implements IAuthorService{
+public class XAuthorService implements IAuthorService{
+
+    @Autowired
+    private IAuthorRepository repository;
 
     @Override
     public IAuthorRepository getRepository() {
         return repository;
     }
 
-    @Autowired
-    private IAuthorRepository repository;
-
-
     @Override
     public List<Author> findAll() {
-
         return (List<Author>) repository.findAll();
     }
 }
