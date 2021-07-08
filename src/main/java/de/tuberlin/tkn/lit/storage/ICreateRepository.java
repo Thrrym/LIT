fa87f912070863e77f2022f522a8de47fb19914b
generalgroovy <41192316+generalgroovy@users.noreply.github.com>
2021-07-08 20:44:24 +0200

    DB Objekte + Create Activity

--- src/main/java/de/tuberlin/tkn/lit/storage/ICreateRepository.java
+++ src/main/java/de/tuberlin/tkn/lit/storage/ICreateRepository.java
@@ -1,9 +1,9 @@
 package de.tuberlin.tkn.lit.storage;
 
-import de.tuberlin.tkn.lit.model.lit.Region;
+import de.tuberlin.tkn.lit.model.activitypub.activities.Create;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IRegionRepository extends CrudRepository<Region, Long> {
+public interface ICreateRepository extends CrudRepository<Create, Long> {
 }
