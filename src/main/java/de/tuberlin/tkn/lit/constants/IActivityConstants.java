e2a3a17c033f84dfc70bf29d2841dc737fcf5eab
generalgroovy <41192316+generalgroovy@users.noreply.github.com>
2021-07-08 21:52:00 +0200

    Konstanten

---
+++ src/main/java/de/tuberlin/tkn/lit/constants/IActivityConstants.java
@@ -0,0 +1,15 @@
+package de.tuberlin.tkn.lit.constants;
+
+public interface IActivityConstants {
+    public static final String ACCEPT = "Accept";
+    public static final String BLOCK = "Block";
+    public static final String CREATE = "Create";
+    public static final String DELETE = "Delete";
+    public static final String DISLIKE = "Dislike";
+    public static final String FOLLOW = "Follow";
+    public static final String IGNORE = "Ignore";
+    public static final String LIKE = "Like";
+    public static final String REJECT = "Reject";
+    public static final String UNDO = "Undo";
+    public static final String UPDATE = "Update";
+}
---
+++ src/main/java/de/tuberlin/tkn/lit/constants/ILitObjectConstants.java
@@ -0,0 +1,15 @@
+package de.tuberlin.tkn.lit.constants;
+
+public interface ILitObjectConstants {
+    public static final String PERSON = "Person";
+
+    public static final String AUTHOR = "Author";
+
+    public static final String BIBTEXARTICLE = "BibTeXArticle";
+    public static final String BOOK = "Book";
+    public static final String JOURNAL = "Journal";
+    public static final String PAPER = "Paper";
+
+    public static final String ACTIVITYPUBCOLLECTION = "ActivityPubCollection";
+
+}
