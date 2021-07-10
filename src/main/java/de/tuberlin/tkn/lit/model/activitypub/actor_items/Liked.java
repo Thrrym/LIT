22faab467874cb3f4132404fb52c874d462ab654
generalgroovy <41192316+generalgroovy@users.noreply.github.com>
2021-07-10 16:17:27 +0200

    Actor objects

---
+++ src/main/java/de/tuberlin/tkn/lit/model/activitypub/actor_items/Liked.java
@@ -0,0 +1,15 @@
+package de.tuberlin.tkn.lit.model.activitypub.actor_items;
+
+import de.tuberlin.tkn.lit.model.activitypub.core.LinkOrObject;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Liked {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @OneToMany(targetEntity = LinkOrObject.class)
+    private List<LinkOrObject> liked;
+}
