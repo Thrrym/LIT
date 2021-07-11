a268f12217d7e87d3499da1ba96af63f987dd5e8
Jonas Hanfland <jonas.hanfland@sablecard.com>
2021-07-11 15:34:07 +0200

    Remove Offer once Accepted/Rejected

--- src/main/java/de/tuberlin/tkn/lit/model/activitypub/activities/Reject.java
+++ src/main/java/de/tuberlin/tkn/lit/model/activitypub/activities/Reject.java
@@ -1,9 +1,12 @@
 package de.tuberlin.tkn.lit.model.activitypub.activities;
 
+import de.tuberlin.tkn.lit.model.activitypub.core.LinkOrObject;
 import de.tuberlin.tkn.lit.storage.IStorage;
 
+import java.util.List;
+
 public class Reject extends Activity {
-    private static final String type = "Activity";
+    private static final String type = "Reject";
 
     public Reject() {
     }
@@ -14,6 +17,13 @@ public class Reject extends Activity {
 
     @Override
     public Activity handle(String actorName,IStorage storage,int port) {
+        Activity offer = storage.getActivity(getObject().getId());
+
+        // Delete rejected Offer from Inbox
+        List<LinkOrObject> inbox = storage.getInbox(actorName).getOrderedItems();
+        LinkOrObject offer_to_delete = inbox.stream().filter(item -> item.getLitObject().getId().equals(offer.getId())).findFirst().get();
+        inbox.remove(offer_to_delete);
+
         return this;
     }
 
