e23cd16a2422ef110e0c473f296c4185b305a0ac
Max Muehlefeldt <max.muehlefeldt@tu-berlin.de>
2021-07-12 15:03:48 +0200

    Reject an offer: Import RejectOffer.vue.

---
+++ frontend/src/components/RejectOffer.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <ServerComRejectOffer
+        ref="ServerComRejectOffer"
+        v-on:requestResponse="requestSuccess"></ServerComRejectOffer>
+    <b-modal ref="rejectSuccessModal" titel="You rejected the offer" ok-only></b-modal>
+  </div>
+</template>
+
+<script>
+import ServerComRejectOffer from "@/components/ServerComRejectOffer";
+
+export default {
+  name: "RejectOffer",
+  components: {
+    ServerComRejectOffer,
+  },
+  data() {
+    return {
+      requestResponse: {},
+      responseText: "",
+      responseJson: {},
+      offerObjects: [],
+    };
+  },
+  methods: {
+    triggerAcceptOffer: function (offerId) {
+      this.$refs.ServerComRejectOffer.trigger(offerId);
+    },
+    requestSuccess: function (response) {
+      this.requestResponse = response;
+      this.responseText = response.responseText;
+      this.responseJson = JSON.parse(this.responseText);
+      this.$refs.acceptSuccessModal.show();
+    },
+  },
+};
+</script>
+
+<style scoped></style>
