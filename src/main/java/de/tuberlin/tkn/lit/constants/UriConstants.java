c1019373ba6c9458e35d43ba1d539f829da0542e
yesoer <71628012+yesoer@users.noreply.github.com>
2021-07-12 03:30:19 +0200

    set protocol sepparately
    name should be called host and host something else

--- src/main/java/de/tuberlin/tkn/lit/constants/UriConstants.java
+++ src/main/java/de/tuberlin/tkn/lit/constants/UriConstants.java
@@ -1,7 +1,9 @@
 package de.tuberlin.tkn.lit.constants;
 
 public final class UriConstants {
-    public static final String HOST = "http://localhost:";
+    public static final String PROTOCOL = "http://";
+    public static final String NAME = "localhost:";
+    public static final String HOST = PROTOCOL + NAME;
     public static final String OUTBOX = "outbox";
     public static final String INBOX = "inbox";
     public static final String FOLLOWING = "following";
